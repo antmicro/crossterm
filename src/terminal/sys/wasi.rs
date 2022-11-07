5a3a60ac0f0360254a602aa29e49fdc48a8d87c2
Grzegorz Placzek <gplaczek@antmicro.com>
2022-11-07 14:16:30 +0100

    Use wasi_ext_lib

--- src/terminal/sys/wasi.rs
+++ src/terminal/sys/wasi.rs
@@ -1,11 +1,8 @@
 //! UNIX related logic for terminal manipulation.
-
-use std::fs;
 use std::io::{Error, ErrorKind};
 
 use crate::error::Result;
 
-use serde_json::json;
 use sscanf::scanf;
 
 pub(crate) fn is_raw_mode_enabled() -> bool {
@@ -14,22 +11,22 @@ pub(crate) fn is_raw_mode_enabled() -> bool {
 }
 
 pub(crate) fn size() -> Result<(u16, u16)> {
-    let buffer = "{ \"args\": [\"get\", \"screenSize\"] }";
-    let custom_syscall = json!({
-        "command": "hterm",
-        "buf_len": buffer.len(),
-        "buf_ptr": format!("{:?}", buffer.as_ptr()),
-    });
-    let hterm_screen = fs::read_link(format!("/!{}", custom_syscall))?;
-    let value = hterm_screen.display().to_string();
-    match scanf!(value, "0\u{1b}[hterm.Size: {}, {}]", u16, u16) {
+    let hterm_screen = match wasi_ext_lib::hterm("screenSize", None) {
+        Ok(s) => s,
+        Err(e) => return Err(Error::new(ErrorKind::Other, format!("Could not get screen size (os error {})", e)))
+    };
+    let value = hterm_screen.unwrap();
+    match scanf!(value, "[hterm.Size: {}, {}]", u16, u16) {
         Ok(size) => Ok(size),
-        Err(_) => Err(
+        Err(e) => {
+            println!("{:?}", e);
+            Err(
                 Error::new(
                     ErrorKind::Unsupported,
                     "Cannot obtain terminal window size with hterm custom syscall"
                 )
-            ),
+            )
+        }
     }
 }
 
